fb831a41b8d56dee79b2aca050a2e5b5207ebda0
MikhailLU <clowfinger13@gmail.com>
2018-04-28 19:46:37 +0300

    footer and results panel markup | adaprive design

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import HomeHeader from "../containers/headers/home/HomeHeader";
+import Footer from "../components/footer/Footer";
+// import Results from "../containers/results/Results";
+// import NoResults from "../containers/results/NoResults";
+import ResultPanel from "../components/resultPanel/ResultPanel";
 
 
 export default class HomePage extends React.Component { // react Component
     render() {
         return (
-            <HomeHeader />
+            <div className="homePage">
+                <HomeHeader />
+                <ResultPanel />
+                {/* <NoResults /> */}
+                <Footer />
+            </div>
         )
     }
 }
