72eb89d1b0dc92f7efc92204dbd004ae5b62b7bc
MikhailLU <clowfinger13@gmail.com>
2018-04-30 13:44:08 +0300

    Error boundary

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -4,16 +4,17 @@ import Footer from "../components/footer/Footer";
 import Results from "../containers/results/Results";
 // import NoResults from "../containers/results/NoResults";
 import ResultPanel from "../components/resultPanel/ResultPanel";
+import ErrorBoundary from "../components/errorBoundary/ErrorBoundary";
 
 
 export default class HomePage extends React.Component { // react Component
     render() {
         return (
             <div className="homePage">
-                <HomeHeader />
-                <ResultPanel />
-                <Results />
-                <Footer />
+                <ErrorBoundary><HomeHeader /></ErrorBoundary>
+                <ErrorBoundary><ResultPanel /></ErrorBoundary>
+                <ErrorBoundary><Results /></ErrorBoundary>
+                <ErrorBoundary><Footer /></ErrorBoundary>
             </div>
         )
     }
